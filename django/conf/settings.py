75fbc90f1816f32df4ee80e42a58f0eea8d88ed9
zukhriddin <zux.828@gmail.com>
2024-08-25 20:45:00 +0500

    update settings

--- django/conf/settings.py
+++ django/conf/settings.py
@@ -18,7 +18,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
